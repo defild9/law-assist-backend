c2f38cd61687043758685e00547add361c7b5c22
Yevhenii Biletskyi <biletskijzena@protonmail.com>
2025-01-03 21:13:42 +0200

    feat(schema): enhance user schema with roles and timestamps
    - Added UserRole type and updated role property.
    - Enabled timestamps for user documents.

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,9 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument, Document } from 'mongoose';
+import { HydratedDocument, Document, Types } from 'mongoose';
+
+export type UserRole = 'user' | 'admin' | 'lawyer';
 
 export type UserDocument = HydratedDocument<User>;
 
-@Schema()
+@Schema({ timestamps: true })
 export class User extends Document {
   @Prop({ required: true, unique: true })
   email: string;
@@ -11,11 +13,23 @@ export class User extends Document {
   @Prop({ required: true })
   password: string;
 
-  @Prop({ required: true, enum: ['client', 'admin'], default: 'client' })
-  userType: 'client' | 'admin';
+  @Prop({
+    required: true,
+    enum: ['user', 'admin', 'lawyer'],
+    default: 'user',
+  })
+  role: UserRole;
 
   @Prop({ default: undefined })
   profile_picture?: string;
+
+  @Prop({ default: null })
+  refreshToken?: string;
+
+  readonly createdAt: Date;
+  readonly updatedAt: Date;
+
+  readonly _id: Types.ObjectId;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);

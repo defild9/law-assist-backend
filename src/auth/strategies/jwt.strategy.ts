5f06ee7d9c061ae0f6be15c6f4a65cfe5f21f6f4
Yevhenii Biletskyi <biletskijzena@protonmail.com>
2025-01-03 21:16:49 +0200

    feat(auth): implement JWT and local authentication strategies
    - Added role decorator for role-based access control.
    - Implemented JWT strategy for token validation.
    - Created local strategy for email/password authentication.
    - Added refresh token strategy for secure token management.

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserService } from 'src/user/user.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtPayload } from '../types/JwtPayload';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly Userervice: UserService,
+    private readonly configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('jwtSecret'),
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const user = await this.Userervice.findById(payload.sub);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return { userId: payload.sub, email: payload.email, role: payload.role };
+  }
+}

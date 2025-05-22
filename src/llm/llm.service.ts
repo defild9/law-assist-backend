b089eb39203e60e9adcc9be7ba3368acb369cb74
Yevhenii Biletskyi <biletskijzena@protonmail.com>
2025-05-22 21:36:47 +0300

    feat(llm): add template generation tool and agent integration
    Introduce a new TemplateGeneratorTool to generate documents from MongoDB templates and integrate it into the LLM service using a reactive agent. This enhances the service's capability to handle dynamic document generation based on user input.

--- src/llm/llm.service.ts
+++ src/llm/llm.service.ts
@@ -1,21 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ChatOpenAI } from '@langchain/openai';
-import { HumanMessage } from '@langchain/core/messages';
+import {
+  HumanMessage,
+  isAIMessageChunk,
+  MessageContentComplex,
+} from '@langchain/core/messages';
 import { VectorStoreService } from 'src/vector-store/vector-store.service';
 import { MessageService } from 'src/message/message.service';
-import { Types } from 'mongoose';
+import { Model, Types } from 'mongoose';
+import { createReactAgent } from '@langchain/langgraph/prebuilt';
+import { MemorySaver } from '@langchain/langgraph';
+import { TemplateGeneratorTool } from './tools/TemplateGenerator.tool';
+import { LegalTemplate } from 'src/schemas/legal-template.schema';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class LlmService {
   private chatModel: ChatOpenAI;
+  private agent: ReturnType<typeof createReactAgent>;
 
   constructor(
     private readonly configService: ConfigService,
     private readonly vectorStoreService: VectorStoreService,
     private readonly messageService: MessageService,
+    @InjectModel(LegalTemplate.name)
+    private readonly templateModel: Model<LegalTemplate>,
   ) {
     this.initializeChatModel();
+    this.initializeAngent();
   }
 
   private initializeChatModel() {
@@ -24,6 +37,16 @@ export class LlmService {
       openAIApiKey: this.configService.get<string>('OPENAI_API_KEY'),
       temperature: 0,
       streaming: true,
+    });
+  }
+
+  private initializeAngent() {
+    const memorySaver = new MemorySaver();
+
+    this.agent = createReactAgent({
+      llm: this.chatModel,
+      tools: [new TemplateGeneratorTool(this.templateModel)],
+      checkpointSaver: memorySaver,
     });
   }
   async *generateStream(
@@ -58,11 +81,25 @@ export class LlmService {
       
       Answer: `;
 
-    const messages = [new HumanMessage(augmentedPrompt)];
-    const stream = await this.chatModel.stream(messages);
+    const streamIter = await this.agent.stream(
+      { messages: [new HumanMessage(augmentedPrompt)] },
+      {
+        streamMode: 'messages',
+        configurable: { thread_id: chatId.toString() },
+      },
+    );
 
-    for await (const chunk of stream) {
-      yield chunk.content;
+    for await (const [msg] of streamIter) {
+      if (!isAIMessageChunk(msg)) continue;
+      if (typeof msg.content === 'string') {
+        yield msg.content;
+      } else {
+        for (const chunk of msg.content as MessageContentComplex[]) {
+          if (chunk.type === 'text') {
+            yield chunk.text;
+          }
+        }
+      }
     }
   }
 }
